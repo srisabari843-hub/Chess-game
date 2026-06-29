8d9205d68b4b76b67009ecd6c5f7df6206b93891
srisabari843-hub <srisabari843@gmail.com>
2026-06-30 01:07:54 +0530

    piece.js

---
+++ pieces.js
@@ -0,0 +1,18 @@
+const PIECES={
+    K:{symbol:'♔',color:'white',name:'King',value:0 },
+    Q:{symbol:'♕', color:'white',name:'Queen',value:9 },
+    R:{symbol:'♖',color:'white',name:'Rook',value:5},
+    B:{symbol:'♗',color:'white',name:'Bishop',value:3},
+    N:{symbol:'♘',color:'white',name:'Kmight',value:3 },
+    P: {symbol:'♙',color:white,name:'Pawn',value:1 },
+
+    k: {symbol:'♚',color:'black',name:'King',value:0 },
+    q: {symbol:'♛',color:'black',name:'Queen',value:9 },
+    r:{symbol:'♜',color:'black',name:'Rook',value:5 },
+    b:{symbol:'♝',color:'black',name:'Bishop',value:3 },
+    n:{symbol:'♞',color:'black',name:'Knight',value:3 },
+    p:{symmbol:'♟',color:'black',name:'Pawn',value:3} ,
+};
+
+
+    
